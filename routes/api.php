8883687677a560146efadfae4bdbff10b5de608d
Toe Oo <bladetoe@gmail.com>
2017-12-06 21:45:47 +0630

    added new routes and sql table structure => "uit_attendance_2017-12-06"

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/v1/attendance', 'AttendanceController@store');
+
+Route::put('/v1/attendance', 'AttendanceController@update');
+
+Route::get('/v1/attendance/{date}/student-list', 'AttendanceController@index');
+
+Route::get('/v1/attendance/{roll_no}/detail', 'AttendanceController@detail');
