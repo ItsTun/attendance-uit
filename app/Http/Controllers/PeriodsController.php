b0ea048ba25c806d99e9fa8a881afd8302695887
Toe Oo <bladetoe@gmail.com>
2017-12-08 13:04:10 +0630

    allow multiple teachers for 1 subject

--- app/Http/Controllers/PeriodsController.php
+++ app/Http/Controllers/PeriodsController.php
@@ -28,13 +28,14 @@ class PeriodsController extends Controller
     	$day = array_search($day, $days);
 
     	$periods = DB::select( DB::raw(
-    		'SELECT periods.period_id, subjects.subject_code, subjects.name subject_name, periods.duration, periods.period_num, periods.room, teachers.name teacher_name
+    		'SELECT periods.period_id, subjects.subject_code, subjects.name subject_name, periods.duration, periods.period_num, periods.room, GROUP_CONCAT(teachers.name) teacher_name
 			FROM periods, subjects, subject_teacher, teachers
 			WHERE subjects.class_id = :class_id 
             AND periods.subject_id = subjects.subject_id 
             AND periods.day = :day
             AND teachers.teacher_id = subject_teacher.teacher_id
             AND subjects.subject_id = subject_teacher.subject_id
+            GROUP BY periods.period_id
 			ORDER BY periods.period_num;'
     	), array('day' => $day, 'class_id' => $klassId) );
 
