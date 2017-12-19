f1cd21a03a5e239448cc948ae008e250c552f717
Ye Min Htut <ymhyeminhtut@gmail.com>
2017-12-19 13:05:04 +0630

    Added logout in teacher route

--- routes/teacher.php
+++ routes/teacher.php
@@ -5,6 +5,7 @@ Route::get('login', 'TeacherController@login');
 Route::group(['middleware' => ['teacher']], function () {
 	Route::get('timetable', 'TeacherController@timetable');
 	Route::get('add', 'TeacherController@addAttendance');
+	Route::get('logout', 'Auth\TeacherLoginController@logout');
 });
 
 Route::get('login/google', 'Auth\TeacherLoginController@redirectToProvider');
