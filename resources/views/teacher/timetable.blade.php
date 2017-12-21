26aea59d5e2ccb6417f13976909399e36bc12ad2
Ye Min Htut <ymhyeminhtut@gmail.com>
2017-12-21 21:00:53 +0630

    [Add] Missing date parameter

--- resources/views/teacher/timetable.blade.php
+++ resources/views/teacher/timetable.blade.php
@@ -35,7 +35,7 @@ Timetable
 		@else
 			@foreach ($timetables as $timetable)
 				<div class="col-12" style="margin-top: 30px;">
-					<a href="add/{{ $timetable->period_id }}" style="color:#67757c;">
+					<a href="add/{{ $timetable->period_id }}?date={{ $selectedDate }}" style="color:#67757c;">
 						<div class="card" style="cursor: pointer;">
 							<div class="card-block">
 								<div class="col-md-3 col-sm-12" style="float: left;">
