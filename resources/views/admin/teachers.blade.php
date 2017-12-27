fbbad1c1f2d90c0479cccb4d74788661ee362356
Aung Kyaw Phyoe <peterphyoe48@gmail.com>
2017-12-27 16:51:13 +0630

    edit teacher view

--- resources/views/admin/teachers.blade.php
+++ resources/views/admin/teachers.blade.php
@@ -9,25 +9,79 @@
 <script type="text/javascript" src="{{ URL::asset('js/adminTeachers.js') }}"></script>
 </head>
     
-<div class="card" style="margin: 5px 5px 5px 5px;">
+<div class="card" style="margin: 7px;">
     <div class="row">
         <div class="col-md-2" style="padding: 30px 0px 0px 40px;">Group By Teacher</div>
         <div class="col-md-4" style="padding: 20px 0px 0px 10px;">
-            <div class="form-group" style="padding: 5px;">
-                <select class="form-control" id="selectfaculty">
+            <div class="form-group form-material" style="padding: 5px;">
+                <select class="form-control form-control-line" id="selectfaculty">
                     <option value="0">Select Faculty</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option value="1">2</option>
+                    <option value="2">3</option>
                 </select>
             </div>
         </div>
         
-        <div class="col-sm" style="padding: 20px 0px 0px 250px;">
+        <div class="col-md" style="padding: 20px 0px 0px 250px;">
             <button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#addTeacher">Add Teacher</button>
         </div>
     </div>
+</div>
+
+
+<div class="card" style="margin: 7px;">
+    <div class="row" style="padding: 30px;">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Subjects</th>
+                    <th>Faculty</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editTeacher">Edit</button>
+                        <button type="button" class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+<!-- Pigination -->
+    <nav class="my-4">
+        <ul class="pagination pagination-circle pg-blue mb-0 justify-content-center">
+            <!--Arrow left-->
+            <li class="page-item disabled">
+                <a class="page-link" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+            </a>
+            </li>
+
+            <!--Numbers-->
+            <li class="page-item active"><a class="page-link">1</a></li>
+            <li class="page-item"><a class="page-link">2</a></li>
+            <li class="page-item"><a class="page-link">3</a></li>
+            <li class="page-item"><a class="page-link">4</a></li>
+            <li class="page-item"><a class="page-link">5</a></li>
+
+            <!--Arrow right-->
+            <li class="page-item">
+                <a class="page-link" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+            </a>
+            </li>
+        </ul>
+    </nav>
 </div>
 
 
@@ -149,62 +203,5 @@
   </div>
 </div>
 <!-- Edit Teacher Modal -->
-
-
-<div class="card" style="margin: 5px 5px 5px 5px;">
-    <div class="row" style="padding: 30px;">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Subjects</th>
-                    <th>Faculty</th>
-                    <th>Options</th>
-                </tr>
-            </thead>
-            <tbody>
-               
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editTeacher">Edit</button>
-                        <button type="button" class="btn btn-danger">Delete</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-<!-- Pigination -->
-    <nav class="my-4">
-        <ul class="pagination pagination-circle pg-blue mb-0 justify-content-center">
-            <!--Arrow left-->
-            <li class="page-item disabled">
-                <a class="page-link" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="sr-only">Previous</span>
-            </a>
-            </li>
-
-            <!--Numbers-->
-            <li class="page-item active"><a class="page-link">1</a></li>
-            <li class="page-item"><a class="page-link">2</a></li>
-            <li class="page-item"><a class="page-link">3</a></li>
-            <li class="page-item"><a class="page-link">4</a></li>
-            <li class="page-item"><a class="page-link">5</a></li>
-
-            <!--Arrow right-->
-            <li class="page-item">
-                <a class="page-link" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="sr-only">Next</span>
-            </a>
-            </li>
-        </ul>
-    </nav>
-</div>
-
 
 @endsection
