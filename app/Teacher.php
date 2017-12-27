5b46fe3a5fc8342bd76ae892d5d78bcedbb240b1
Aung Kyaw Phyoe <peterphyoe48@gmail.com>
2017-12-27 13:15:37 +0630

    added teacher model

--- app/Teacher.php
+++ app/Teacher.php
@@ -14,46 +14,60 @@ class Teacher extends Model
     	return $this->hasMany(Subject_Teacher::class, 'teacher_id');
     }
     
-    public function getteacher($faculty) {
-        return DB::table('teachers') 
-            ->join('subject_teacher', 'subject_teacher.teacher_id', '=', 'teachers.teacher_id') 
-            ->join('subjects', 'subjects.subjects_id', '=', 'subject_teacher.subject_id') 
-            ->where('teachers.faculty', $faculty)
-            ->select('teachers.*','subjects.name')
-            ->groupby('faculty')
+    public function getallteacher(){
+        return DB::table('teacher')
+            ->join('subject_class_teacher', 'subject_class_teacher.teacher_id', '=', 'teachers.teacher_id') 
+            ->join('subjects_class', 'subjects_class.subject_id', '=', 'subject_class_teacher.subject_id') 
+            ->join('subjects', 'subjects.subject_id', '=', 'subject_class.subject_id') 
+            ->select('teachers.*','subjects.name as subj_name')
+            ->groupby('faculty_id')
             ->orderby('teacher_id')
             ->get();
     }
     
-    public function addteacher($name, $email, $faculty_id, $subj_id) {
-        $result = DB::insert( DB::raw (
-            'INSERT INTO teachers (name, email, faculty_id, s.subject_id) 
-            VALUE (:name, :email, :faculty_id, :subj_id)
-            FROM teachers INNER JOIN subject_teacher s 
-            ON teachers.teacher_id=s.teacher_id'
-        ),array('name' => $name, 'email' => $email, 'faculty_id' => $faculty_id, 'subj_id' => $subj_id) );
-        
-        return $result;
+    public function getteacher($faculty_id) {
+        return DB::table('teachers') 
+            ->join('subject_class_teacher', 'subject_class_teacher.teacher_id', '=', 'teachers.teacher_id') 
+            ->join('subjects_class', 'subjects_class.subject_id', '=', 'subject_class_teacher.subject_id') 
+            ->join('subjects', 'subjects.subject_id', '=', 'subject_class.subject_id') 
+            ->where('teachers.faculty_id', $faculty_id) 
+            ->select('teachers.*','subjects.name as subj_name') 
+            ->groupby('faculty_id')
+            ->orderby('teacher_id')
+            ->get();
     }
     
-    public function updateteacher($teacher_id, $name, $email, $faculty_id, $subj_id) {
-        $result = DB::update( DB::raw (
-            'UPDATE teachers,subject_teacher 
-            SET teachers.name= :name, teachers.email= :email, teachers.faculty_id= :faculty_id, subject_teacher.subject_id= :subj_id 
-            FROM teachers t, subject_teacher s 
-            WHERE t.teacher_id=s.teacher_id 
-            AND t.teacher_id=:teacher_id'
-        ), array('teacher_id' => $teacher_id, 'name' => $name, 'email' => $email, 'faculty_id' => $faculty_id, 'subj_id' => $subj_id) );
+    public function addteacher($name, $email, $faculty_id, $subject_class_ids) {
+        $teacher = new Teacher();
+        $teacher->name = $name;
+        $teacher->email = $email;
+        $teacher->faculty_id = $faculty_id;
+        $teacher->save();
         
-        return $result;
+        $teacher = Teacher::where('email', $email)->first();
+        $teacher_id = $teacher->teacher_id;
+        
+        foreach ($subject_class_ids as $subject_class_id) {
+            $subject_teacher = new Subject_Teacher();
+            $subject_teacher->subject_class_id = $subject_class_id;
+            $subject_teacher->teacher_id = $teacher_id;
+            $subject_teacher->save();
+        }
+        
+                                  
     }
     
-    public function deleteteacher($teacher_id) {
-        $result = DB::delete( DB::raw (
-            'DELETE t.*, s.*  
-            FROM teachers t 
-            INNER JOIN subject_teacher s ON t.teacher_id = s.teacher_id 
-            WHERE t.teacher_id = :teacher_id'
-        ), array('teacher_id' => $teacher_id) );
+    public function updateteacher($teacher_id, $name, $email, $faculty_id, $subject_class_ids) {
+        DB::table('teachers')
+            ->whereIn('teacher_id', $teacher_id) 
+            ->update(['name' => $name], ['email' => $email], ['faculty_id' => $faculty_id]);
+        
+        foreach ($subject_class_ids as $subject_class_id){
+        DB::table('subject_class_teacher')
+            ->whereIn('teacher_id',$teacher_id ) 
+            ->update(['subject_class_id' => $subject_class_id])
+        }
+    }
+        
     }
 }
