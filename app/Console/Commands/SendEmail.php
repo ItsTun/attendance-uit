b6d4e0c8c759c06854455a3fa4b089998119f09c
Toe Oo <bladetoe@gmail.com>
2018-02-04 14:01:25 +0630

    [Add] send emails set up

---
+++ app/Console/Commands/SendEmail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Klass;
+
+use Illuminate\Console\Command;
+
+class SendEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send emails to associated teachers for many absent students.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo 'testing ...';
+        // send mail to all teachers and admins
+    }
+}
